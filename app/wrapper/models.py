564b641b0f2fbe3d325f44ca2d26ea52c08b039e
Pavel Minenkov <minikspb@gmail.com>
2020-09-07 11:24:34 +0300

    db migastion

--- app/wrapper/models.py
+++ app/wrapper/models.py
@@ -6,3 +6,8 @@ class Ledger(models.Model):
     name = models.CharField(max_length=512, db_index=True)
     metadata = JSONField(null=True, default=None)
 
+
+class Transaction(models.Model):
+    ledger = models.ForeignKey(Ledger, on_delete=models.CASCADE)
+    txn = JSONField()
+    metadata = JSONField(null=True, default=None)
