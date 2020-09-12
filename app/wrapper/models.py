53c971e2f3ccf28d176cc7c35e453e1576036640
Pavel Minenkov <minikspb@gmail.com>
2020-09-12 12:58:04 +0300

    db migration

--- app/wrapper/models.py
+++ app/wrapper/models.py
@@ -3,11 +3,19 @@ from django.contrib.postgres.fields import JSONField
 
 
 class Ledger(models.Model):
+    entity = models.CharField(max_length=64, db_index=True, null=True)
     name = models.CharField(max_length=512, db_index=True)
     metadata = JSONField(null=True, default=None)
+
+    class Meta:
+        unique_together = ('entity', 'name')
 
 
 class Transaction(models.Model):
     ledger = models.ForeignKey(Ledger, on_delete=models.CASCADE)
     txn = JSONField()
+    seq_no = models.IntegerField(null=True)
     metadata = JSONField(null=True, default=None)
+
+    class Meta:
+        unique_together = ('seq_no', 'ledger')
