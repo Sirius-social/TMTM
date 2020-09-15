bde85808d35d32a3b72f2d5404ae380f80399fd5
Pavel Minenkov <minikspb@gmail.com>
2020-09-16 00:26:33 +0300

    db migration

--- app/wrapper/models.py
+++ app/wrapper/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import JSONField, ArrayField
 
 
 class Ledger(models.Model):
     entity = models.CharField(max_length=64, db_index=True, null=True)
     name = models.CharField(max_length=512, db_index=True)
     metadata = JSONField(null=True, default=None)
+    participants = ArrayField(models.CharField(max_length=128), null=True)
 
     class Meta:
         unique_together = ('entity', 'name')
