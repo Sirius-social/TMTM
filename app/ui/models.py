47147ede194ef2ecaa4edd93abf4163e005fefd8
Pavel Minenkov <minikspb@gmail.com>
2020-09-23 17:36:44 +0300

    db migration

--- app/ui/models.py
+++ app/ui/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class QRCode(models.Model):
+    connection_key = models.CharField(max_length=64, db_index=True)
+    url = models.CharField(max_length=2048)
