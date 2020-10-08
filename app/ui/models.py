0ab2552da981d9b1afe7eb5e504d4f6ce2745e35
Pavel Minenkov <minikspb@gmail.com>
2020-10-09 01:18:27 +0300

    db migration

--- app/ui/models.py
+++ app/ui/models.py
@@ -6,3 +6,9 @@ class QRCode(models.Model):
     connection_key = models.CharField(max_length=64, db_index=True)
     my_endpoint = JSONField(null=True)
     url = models.CharField(max_length=2048, db_index=True)
+
+
+class PairwiseRecord(models.Model):
+    entity = models.CharField(max_length=64)
+    their_did = models.CharField(max_length=64, db_index=True)
+    metadata = JSONField()
